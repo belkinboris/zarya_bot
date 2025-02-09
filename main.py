19b0910ae75c6b2280d754b4567400c6b43f4ec4
belkinboris <belkinboris95@gmail.com>
2025-02-09 03:53:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [
             InlineKeyboardButton(
                 text="В Зарю 🌅",
-                web_app=WebAppInfo(url="https://t.me/Zarya_Hub_Bot?startapp=1")
+                web_app=WebAppInfo(url="belkinboris.github.io/zarya")
                 # ВАЖНО: URL должен совпадать с доменом, указанным в BotFather /setdomain
             )
         ]
